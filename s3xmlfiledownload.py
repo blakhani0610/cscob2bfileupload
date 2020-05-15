eac03a9f073d5ad13f2f29cff33b434c27a24f22
blakhani0610 <65420404+blakhani0610@users.noreply.github.com>
2020-05-15 12:34:22 -0700

    Sample upload download script to AWS S3.

---
+++ s3xmlfiledownload.py
@@ -0,0 +1,31 @@
+import os
+import boto3
+import glob
+from botocore.client import Config
+
+
+ACCESS_KEY_ID = 'AKIAQEOLLI5ME6FUN5HP'
+ACCESS_SECRET_KEY = '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'
+BUCKET_NAME = 'aws-demo2'
+
+#data = open('CloudHomework.jpg', 'rb')
+
+s3 = boto3.resource(
+    's3',
+    aws_access_key_id=ACCESS_KEY_ID,
+    aws_secret_access_key=ACCESS_SECRET_KEY,
+    config=Config(signature_version='s3v4')
+)
+
+
+
+s3=boto3.client('s3')
+list=s3.list_objects(Bucket=BUCKET_NAME)['Contents']
+for key in list:    
+      s3.download_file(BUCKET_NAME, key['Key'], key['Key'])
+      print("Downloaded file", key)
+      s3.delete_object(Bucket=BUCKET_NAME,Key=key['Key'])
+      continue
+    
+
+print ("Done") 
---
+++ s3xmlfileupload.py
@@ -0,0 +1,33 @@
+import os
+import boto3
+import glob
+from botocore.client import Config
+
+
+ACCESS_KEY_ID = '*****************'
+ACCESS_SECRET_KEY = '**********************************'
+BUCKET_NAME = 'yourbucket'
+
+#data = open('CloudHomework.jpg', 'rb')
+
+s3 = boto3.resource(
+    's3',
+    aws_access_key_id=ACCESS_KEY_ID,
+    aws_secret_access_key=ACCESS_SECRET_KEY,
+    config=Config(signature_version='s3v4')
+)
+
+
+for filename in glob.glob('*.xml'):
+    if filename.endswith(".xml"):
+        data = open(filename, 'rb') 
+        s3.Bucket(BUCKET_NAME).put_object(Key=filename, Body=data)
+        print("Uploaded" ,os.path.join(os.getcwd(), filename))
+        #os.remove(filename)
+        continue
+    else:
+        print("No more files to upload")
+
+#s3.Bucket(BUCKET_NAME).put_object(Key='CloudHomework.jpg', Body=data)
+
+print ("Done")
